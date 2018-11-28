562b40b86c549af70560f2e7ed1dad6d06e0b77c
Zack Fornaca <zfornaca@gmail.com>
2018-11-28 13:50:37 -0800

    forgot to commit cors addition yesterday

--- yelp-fuzzy-react/src/CardContainer.js
+++ yelp-fuzzy-react/src/CardContainer.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Card from './Card';
-import './App.css';
-import token from './token.js';
+import axios from 'axios';
+// import TOKEN from './token.js';
 
-const axios = require('axios');
+// const config = {
+//   headers: { Authorization: 'Bearer ' + TOKEN },
+//   params: {
+//     term: 'bulgoki',
+//     location: 'Oakland'
+//   }
+// };
 
 class CardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.data = {};
+  }
+
+  componentWillMount() {
+    console.log('hello my buddy my pal');
+    axios.get('http://localhost:3001').then(res => console.log(res.data));
+  }
+
   render() {
     return (
       <div className="CardContainer">
@@ -22,6 +38,12 @@ https://api.yelp.com/v3/businesses/search?term=bulgoki&location=Oakland
 Auth.
 bearer
 Bearer TESTING
+
+axios.get('https://api.yelp.com/v3/businesses/search?term=bulgoki&location=Oakland
+', {
+  headers: {'Authorization': "bearer " + TOKEN}
+})
+
 
 
 */
