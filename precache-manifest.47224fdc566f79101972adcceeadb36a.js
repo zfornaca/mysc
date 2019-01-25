34419a17e2bd694399cb2013a5a52c1e971af402
Zack Fornaca <zfornaca@gmail.com>
2019-01-25 15:59:00 -0800

    Updates

---
+++ precache-manifest.47224fdc566f79101972adcceeadb36a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40f7eae6e5a3e422102b",
+    "url": "/mysc/static/js/runtime~main.40f7eae6.js"
+  },
+  {
+    "revision": "b8a409c4f97214126ad2",
+    "url": "/mysc/static/js/main.b8a409c4.chunk.js"
+  },
+  {
+    "revision": "5101419c53b2a073e929",
+    "url": "/mysc/static/js/1.5101419c.chunk.js"
+  },
+  {
+    "revision": "b8a409c4f97214126ad2",
+    "url": "/mysc/static/css/main.27e69932.chunk.css"
+  },
+  {
+    "revision": "7317c781a2e2f9ec9ad397191c5c6f74",
+    "url": "/mysc/index.html"
+  }
+];
