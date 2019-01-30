db5bff75343ead5e952ae5642a14103cab0fc5b0
Zack Fornaca <zfornaca@gmail.com>
2019-01-30 15:18:18 -0800

    Updates

--- precache-manifest.185a6f17fcfee489e07eb6fc88c3f3ef.js
+++ precache-manifest.185a6f17fcfee489e07eb6fc88c3f3ef.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mysc/static/js/runtime~main.40f7eae6.js"
   },
   {
-    "revision": "d31f5ac74861514ec733",
-    "url": "/mysc/static/js/main.d31f5ac7.chunk.js"
+    "revision": "029b51172376dcf2493c",
+    "url": "/mysc/static/js/main.029b5117.chunk.js"
   },
   {
     "revision": "5101419c53b2a073e929",
     "url": "/mysc/static/js/1.5101419c.chunk.js"
   },
   {
-    "revision": "d31f5ac74861514ec733",
-    "url": "/mysc/static/css/main.a20592c0.chunk.css"
+    "revision": "029b51172376dcf2493c",
+    "url": "/mysc/static/css/main.5402b56c.chunk.css"
   },
   {
-    "revision": "2c51953e2c08a43e298d99b61d13eacf",
+    "revision": "bd37fba38080ff9cab22e3ee88572217",
     "url": "/mysc/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mysc/precache-manifest.0c60b16fa753587faec9aaf101ec5701.js"
+  "/mysc/precache-manifest.185a6f17fcfee489e07eb6fc88c3f3ef.js"
 );
 
 workbox.clientsClaim();
